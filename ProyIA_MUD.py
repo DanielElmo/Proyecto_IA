c8011d735797571b7b3b2807396b042188bd4e0a
DanielMU <42394729+DanielElmo@users.noreply.github.com>
2021-11-30 20:57:30 -0600

    Update ProyIA_MUD.py

--- ProyIA_MUD.py
+++ ProyIA_MUD.py
@@ -6,3 +6,9 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from apyori import apriori
+
+Hipoteca = pd.read_csv('Hipoteca.csv', header=0)
+Movies = pd.read_csv('Movies.csv', header=0)
+RGeofisicos = pd.read_csv('RGeofisicos.csv', header=0)
+StoreData = pd.read_csv('store_data.csv', header=0)
+WDBCOriginal = pd.read_csv('WDBCOriginal.csv', header=0)
